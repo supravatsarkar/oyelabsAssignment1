46e089e1c76409dd4109687674b70035d7c37ef4
unknown <Supravat Sarkar>
2022-03-28 18:31:14 +0530

    connect with mongodb

--- index.js
+++ index.js
@@ -2,14 +2,37 @@ const express = require('express');
 const { userInfo } = require('os');
 const app = express();
 const port = 5000;
+require('dotenv').config()
+const { MongoClient } = require("mongodb");
 
-const adminUsers = [
-    {
-        mobileNo: '9876543210',
-        name: 'admin1',
-        password: '123456',
+
+const uri =
+    `mongodb+srv://${process.env.DB_USER_ID}:${process.env.DB_PASSWORD}@cluster0.m4rht.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+// console.log(uri);
+// create new mongo clients 
+const client = new MongoClient(uri);
+
+async function run() {
+    try {
+        await client.connect();
+
+        const database = client.db('oyelabs_assignment');
+        const movies = database.collection('adminUsers');
+
+        // Query for a movie that has the title 'Back to the Future'
+        const query = { title: 'Back to the Future' };
+        const movie = await movies.findOne(query);
+
+        // console.log(movie);
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
     }
-]
+}
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
     console.log('Server running ok...');
